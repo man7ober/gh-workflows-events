a86d342b1551912a9dda9f44c42fd74684275452
man7ober <man7ober@gmail.com>
2024-10-04 10:37:37 +0530

    added an h5 tag to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ function App() {
   return (
     <>
       <header>
-        <div id="logo-img">
+        <div id='logo-img'>
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <h5>Academind - Maximillian Schwarzmueller</h5>
       </header>
       <MainContent />
     </>
